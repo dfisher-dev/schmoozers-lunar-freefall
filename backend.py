8d0de921b27663bfa1676bfa6cb61d88983818d0
Thomas Ciola <tciola001@student.franciscan.edu>
2025-01-31 13:15:51 -0500

    Added mission_start table and fixed database setup

--- backend.py
+++ backend.py
@@ -2,3 +2,17 @@ import sqlite3
 
 conn = sqlite3.connect('backend.db')
 cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS mission_start (
+        mission_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        start_height REAL,
+        start_fuel REAL,
+        thrust_power REAL,
+        gravity REAL DEFAULT 1.62,
+        time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+''')
+
+conn.commit()
+conn.close()
