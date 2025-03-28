afa1ed3d218fe405814a3ba7ec9a2fe8844e2370
Drewski228 <dvanloenen001@student.franciscan.edu>
2025-03-28 11:27:08 -0400

    Current files needed to run the GUI
    These are all the images I have so far for being able to run the GUI you need to have all of the images in the working directory

--- Updated GUI2.py
+++ Updated GUI2.py
@@ -5,26 +5,29 @@ pygame.init()
 width=1280
 height=720
 screen = pygame.display.set_mode((1280, 720))
-bg_img=pygame.image.load('Software Arch/Surface.png')
+bg_img=pygame.image.load('Surface.png')
 bg=pygame.transform.scale(bg_img,(1280,720))
 #Menu Set Up
 MENU = pygame.display.set_mode((1280,720))
-MENU_img=pygame.image.load('Software Arch/MENU.png')
+MENU_img=pygame.image.load('MENU.png')
 MENU=pygame.transform.scale(MENU_img,(500,500))
 #Setting Fps for time
 clock = pygame.time.Clock()
 #Controller image
-controller_img=pygame.image.load('Software Arch/Gizmo.png')
+controller_img=pygame.image.load('Gizmo.png')
 controller=pygame.transform.scale(controller_img,(360,360))
 #Moon
-moon_img=pygame.image.load('Software Arch/Moon.png')
+moon_img=pygame.image.load('Moon.png')
 Moon=pygame.transform.scale(moon_img,(1280,720))
 #Lander 
-lander_img=pygame.image.load('Software Arch/The Lander.png')
+lander_img=pygame.image.load('The Lander.png')
 lander=pygame.transform.scale(lander_img,(200,200))
 #thruster Fire
-Fire_img=pygame.image.load('Software Arch/Fire.png')
+Fire_img=pygame.image.load('Fire.png')
 Fire=pygame.transform.scale(Fire_img,(200,200))
+#kaboom 
+Kaboom_img=pygame.image.load('Kaboom.png')
+Kaboom=pygame.transform.scale(Kaboom_img,(100,100))
 #Setting up the ability to draw text on the screen
 text_font=pygame.font.SysFont("Arial",30)
 text_font2=pygame.font.SysFont("Arial",110,True)
@@ -46,6 +49,7 @@ G=0.5
 #For the screen scrolling
 i=0
 j=0
+z=0
 #Thrust
 Thrust=False
 time_s=int(0)
@@ -68,7 +72,6 @@ Frames=0
    # screen.blit(MENU,(375,200))
    # pygame.display.update()
 
-#turned the lander sim into a function so it just needs to be called
 while running:
     clock.tick(60)
     # poll for events
@@ -85,7 +88,7 @@ while running:
     if Thrust==True:
         if Fuel>0:
             Fuel-=0.05
-            ly-=3
+            ly-=0.75
     #Timer
     Frames+=1
     if Frames==60:
@@ -99,17 +102,24 @@ while running:
     if ly<100:
         screen.blit(bg,(i,j))
         screen.blit(bg,(width+i,j))
+        
     if ly>100:
-        screen.blit(bg,(i,j))
-        screen.blit(bg,(width+i,j))
+        if z==0:
+            screen.blit(bg,(i,j))
+            screen.blit(bg,(width+i,j))
+        if z==1:
+            screen.blit(Moon,(i,j))
+            screen.blit(Moon,(width+i,j))
         screen.blit(Moon,(i,height+j))
         screen.blit(Moon,(width+i,height+j))
+        
         j-=0.5
     screen.blit(Moon,(i,height+j))
     screen.blit(Moon,(width+i,height+j))
     screen.blit(controller,(0,360))
     if Thrust==True:
-        screen.blit(Fire,(805,ly+25))
+        if Fuel > 0:
+            screen.blit(Fire,(805,ly+25))
     screen.blit(lander,(800,ly))
     if event.type==pygame.KEYDOWN:
         if event.type==pygame.K_SPACE:
@@ -127,13 +137,20 @@ while running:
     draw_text(str(int(velocity)),text_font,(0,205,100),200,505)
     Percent_mover()
     if i==-width:
-        screen.blit(bg,(width+1,j))
-        i=0
+       screen.blit(Moon,(width+1,j))
+       i=0
+       z=1
     if j==-height:
-        screen.blit(bg,(i,height+1))
-        j=0
+       screen.blit(Moon,(i,height+1))
+       j=0
+       z=1
     i-=0.5
     ly+=G
+    print (j)
+    if h==0:
+        if velocity>5:
+            screen.blit(Kaboom,(800,ly))
+
 
 
     pygame.display.update()
